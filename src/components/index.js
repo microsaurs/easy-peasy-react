a137256a3b731009d629980c8e0f194d20d3ad66
fhwang98 <fhwang9815@gmail.com>
2024-07-14 18:00:58 +0900

    css파일 이름 수정

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as MainComponent } from "./Main/MainComponent";
 export { default as FileImportComponent } from "./FileImport/FileImportComponent";
+export { default as ServiceComponent } from "./Service/ServiceComponent";
